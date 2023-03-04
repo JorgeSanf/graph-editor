d823024b1ebbb10fd531088a5a0133aae902e1cc
JorgeSanf <72420626+JorgeSanf@users.noreply.github.com>
2023-03-04 01:40:05 +0100

    Menú de configuración condicional en base al state

--- imports/ui/components/Configuration.jsx
+++ imports/ui/components/Configuration.jsx
@@ -8,42 +8,78 @@ import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import ToggleButton from "@mui/material/ToggleButton";
+import { Session } from 'meteor/session';
+import { Tracker } from 'meteor/tracker';
+import { useTracker } from 'meteor/react-meteor-data';
+import Slider from '@mui/material/Slider';
+import * as fs from 'fs';
+import { Meteor } from 'meteor/meteor';
 
-export const Config = () => {
-
+const Toggle = () => {
     const [alignment, setAlignment] = React.useState('left');
 
     const handleAlignment = (event, newAlignment) => {
         setAlignment(newAlignment);
     };
 
-    return <Grid>
-        <Box sx={{ m: 2 }}>
+
+    return <Stack direction="row" spacing={1}>
+        <ToggleButtonGroup
+            value={alignment}
+            exclusive
+            onChange={handleAlignment}
+            aria-label="text alignment"
+        >
+            <ToggleButton value="left" aria-label="left aligned">
+                1
+            </ToggleButton>
+            <ToggleButton value="center" aria-label="centered">
+                2
+            </ToggleButton>
+            <ToggleButton value="right" aria-label="right aligned">
+                3
+            </ToggleButton>
+            <ToggleButton value="hard" aria-label="left aligned">
+                4
+            </ToggleButton>
+        </ToggleButtonGroup>
+    </Stack>
+}
+
+export const Config = () => {
+    const [menu, setMenu] = React.useState(0);
+
+    const chartIndex = useTracker(() => Session.get('chartIndex'));
+
+    React.useEffect(() => {
+        setMenu(chartIndex);
+    }, [chartIndex]);
+
+    return (
+        <Grid>
+
             <Typography gutterBottom variant="body1">
                 Select type
             </Typography>
+            <Box sx={{ m: 2, display: "flex" }}>
+                {menu ? <Toggle /> : ''}
+                <Slider
+                    defaultValue={50}
+                    aria-label="Default"
+                    valueLabelDisplay="auto"
+                    sx={{ width: '10%' }}
+                />
+            </Box>
+        </Grid>
+    );
+};
 
-            <Stack direction="row" spacing={1}><ToggleButtonGroup
-                value={alignment}
-                exclusive
-                onChange={handleAlignment}
-                aria-label="text alignment"
-            >
-                <ToggleButton value="left" aria-label="left aligned">
-                    <Chip label="Extra Soft" />
-                </ToggleButton>
-                <ToggleButton value="center" aria-label="centered">
-                    <Chip color="primary" label="Soft" />
-                </ToggleButton>
-                <ToggleButton value="right" aria-label="right aligned">
-                    <Chip label="Medium" />
-                </ToggleButton>
-                <ToggleButton value="hard" aria-label="left aligned">
-                    <Chip label="Hard" />
-                </ToggleButton>
-            </ToggleButtonGroup>
-
-            </Stack>
-        </Box>
-    </Grid>
-}
+/*function retrieveOptions() {
+    const chartIndex = Session.get('chartIndex');
+    console.log(chartIndex);
+    const mock = require('./../../../tests/mock.json');
+    console.log(mock);
+    const charType = mock[chartIndex];
+    console.log(charType);
+    return charType;
+}*/
